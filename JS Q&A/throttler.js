1cc2c2915541de0b61f42528f1b3f358b9bc20a6
Rohit <stylishrb711@gmail.com>
2022-07-02 21:46:14 +0530

    JS Q&A added

---
+++ JS Q&A/throttler.js
@@ -0,0 +1,13 @@
+function throttler(func, delay) {
+  let prev = 0;
+  return (...args) => {
+    let now = new Date().getTime();
+    console.log(now - prev, delay);
+    if (now - prev > delay) {
+      prev = now;
+      return func(...args);
+    }
+  };
+}
+
+btn.addEventListener("click", throttler(callback, 1500));
