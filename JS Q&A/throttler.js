243abdad503c57fe3be46f8205c67c8fefb99fbe
Rohit <stylishrb711@gmail.com>
2022-07-02 21:47:26 +0530

    throttler update

--- JS Q&A/throttler.js
+++ JS Q&A/throttler.js
@@ -2,7 +2,7 @@ function throttler(func, delay) {
   let prev = 0;
   return (...args) => {
     let now = new Date().getTime();
-    console.log(now - prev, delay);
+    // console.log(now - prev, delay);
     if (now - prev > delay) {
       prev = now;
       return func(...args);
