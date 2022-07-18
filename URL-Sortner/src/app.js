0b8ec22a7215030ba8d87f519906928394a072c2
Rohit <stylishrb711@gmail.com>
2022-07-18 20:09:19 +0530

    new ass ignment added

---
+++ URL-Sortner/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server start running on port ${port}.`);
+});
