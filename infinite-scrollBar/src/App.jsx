73ba79b45dabd12d61e31be8b59790e13e32684b
Rohit <stylishrb711@gmail.com>
2022-07-19 19:04:48 +0530

    new assignment added

---
+++ infinite-scrollBar/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Infinite Scroll Bar</h1>
+    </div>
+  );
+}
+
+export default App;
